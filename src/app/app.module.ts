f913bc41626f920ca540b03269f06f16806ac5ba
jktechs <jjlkoppe@hotmail.com>
2023-01-10 22:47:48 +0100

    update2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 import { AppComponent } from './app.component';
@@ -21,7 +21,7 @@ import { GradeWidgetComponent } from './components/grade-widget/grade-widget.com
 import { WidgetComponent } from './components/widget/widget.component';
 import { SubjectListPageComponent } from './components/subject-list-page/subject-list-page.component';
 import { KwtOptionPageComponent } from './components/kwt-option-page/kwt-option-page.component';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
 
 @NgModule({
   declarations: [
